13f50fc469315ee0620978f7e490ef3ed1066f9f
Chen Asraf <casraf@pm.me>
2024-12-24 03:52:04 +0200

    feat: update sofmani to v1.0.1

--- Formula/sofmani.rb
+++ Formula/sofmani.rb
@@ -1,8 +1,8 @@
 class Sofmani < Formula
   desc "Installs software from a declerative config on any system"
   homepage "https://github.com/chenasraf/sofmani"
-  url "https://github.com/chenasraf/sofmani/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "63a5c65114b47690481b99efc822a3d0034038af5819f06f997083bc9316b66a"
+  url "https://github.com/chenasraf/sofmani/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "451cf51f9b11887f072cc9d5fe7e3640adbb5638852c84613effaaa3df875f73"
   license "CC0-1.0"
 
   bottle do
