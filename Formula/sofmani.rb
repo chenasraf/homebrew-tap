fb2e25128deb5998b0c693fc54e395b29f678cb4
Chen Asraf <casraf@pm.me>
2024-12-24 03:41:02 +0200

    feat: add sofmani v1.0.0

---
+++ Formula/sofmani.rb
@@ -0,0 +1,18 @@
+class Sofmani < Formula
+  desc "Installs software from a declerative config on any system"
+  homepage "https://github.com/chenasraf/sofmani"
+  url "https://github.com/chenasraf/sofmani/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "63a5c65114b47690481b99efc822a3d0034038af5819f06f997083bc9316b66a"
+  license "CC0-1.0"
+
+  depends_on "go" => [:build]
+
+  def install
+    system "go", "build", "-buildmode", "exe", "-o", "sofmani", "."
+    bin.install "sofmani"
+  end
+
+  test do
+    assert_match "Usage: sofmani [options] [config_file]", shell_output("#{bin}/sofmani -h")
+  end
+end
