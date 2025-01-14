3cba87c390aad4c68388da72e3898815d39b4904
Chen Asraf <casraf@pm.me>
2025-01-14 09:39:43 +0200

    chore: update script

--- scripts/create-app-update.sh
+++ scripts/create-app-update.sh
@@ -7,13 +7,14 @@ fi
 VERSION=$(curl -s "https://api.github.com/repos/chenasraf/$REPO_NAME/releases/latest" | jq -r .tag_name)
 
 echo "Version: $VERSION"
+BRANCH="feature/$REPO_NAME-$VERSION"
 
-if ! git switch -C "feature/$REPO_NAME-$VERSION"; then
+if ! git switch -C "$BRANCH"; then
   echo "Branch already exists, aborting"
 fi
 git add "Formula/$REPO_NAME.rb"
 git commit -m "feat: update $REPO_NAME to $VERSION"
-git push origin
+git push --set-upstream origin "$BRANCH"
 
 if gh pr create --fill; then
   echo open -u "$(gh pr list --json url | jq -r '.[0].url')"
