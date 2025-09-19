8bd6658c39924af926410eff349ba1a2d60a4366
Chen Asraf <contact@casraf.dev>
2025-09-19 12:41:27 +0000

    vstask: update 1.0.0 bottle.

--- Formula/vstask.rb
+++ Formula/vstask.rb
@@ -6,10 +6,10 @@ class Vstask < Formula
   license "CC0-1.0"
 
   bottle do
-    root_url "https://github.com/chenasraf/homebrew-tap/releases/download/vstask-0.0.0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "f0eb5cbbe6765bcf59c7b0252ad209319c6af87a22ca3394b16e313433f7c055"
-    sha256 cellar: :any_skip_relocation, ventura:      "7b3a1cc3813bcf10989d62f0152e691d0bf90e89a91b4ce7db3b59e1a8dc0c20"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "505b7b89adca5b8305680be81f543149b0130f21fd1170ebde4cfb10d6efb50a"
+    root_url "https://github.com/chenasraf/homebrew-tap/releases/download/vstask-1.0.0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "7a6cf8b5b1b0166c2b0d945b571336f22803967e94bb6eb96536be661abf42ed"
+    sha256 cellar: :any_skip_relocation, ventura:      "de076ba1c9c62d9277858a9f215cfdcdeefec8128c702017016e6c0f4a86c835"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "58cf5ee6eb3454a9c8090f841511b3f2448ad54b16e12f207893a669818c8a16"
   end
 
   depends_on "go" => [:build]
