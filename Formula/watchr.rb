fdd4b7d2b1f620bdc49c0ea5fc50d60bc55b0532
Chen Asraf <casraf@pm.me>
2025-12-03 14:34:10 +0200

    fix: watchr cellar sha

--- Formula/watchr.rb
+++ Formula/watchr.rb
@@ -7,7 +7,7 @@ class Watchr < Formula
 
   bottle do
     root_url "https://github.com/chenasraf/homebrew-tap/releases/download/watchr-1.0.0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "728f8a1.0.0c6f0afc0f0c7f37352e3faf12cbdcaa8eeb9bb86d4d0b6c5f5bd2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "728f8a101e1c6f0afc0f0c7f37352e3faf12cbdcaa8eeb9bb86d4d0b6c5f5bd2"
     sha256 cellar: :any_skip_relocation, ventura:      "51fbc2d85f8e4fb01f3874db88da74b12d1fa227c83f72d088d0c2956264bb67"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "5abace5d36a881b5ca59b221916393b38879ef7acf43a9e3626eaa2b4c25a790"
   end
