f54accc082fcff0d72a1966929a1dd58feb9f4b0
Chen Asraf <contact@casraf.dev>
2026-01-29 09:43:49 +0000

    tx: add 1.0.0 bottle.

--- Formula/tx.rb
+++ Formula/tx.rb
@@ -5,6 +5,13 @@ class Tx < Formula
   sha256 "39eb5028298c2a50c77f74f206ce2d63c11e40fe69bf6016f82cdd49cf5b8542"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/chenasraf/homebrew-tap/releases/download/tx-1.0.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "de1748424aa1c52d6c5adfcfef365e1445a1da44609555fd8e2eacc69aadf6d5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a0c4d879f5ca458339c2730ef0fc8538f64d1ab3b4cb5bc950567abf3f8950fe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "78b20c79c167295300529881628dacdbdb7d8161507c3f3b08333e9c7083f63e"
+  end
+
   depends_on "go" => [:build]
 
   def install
