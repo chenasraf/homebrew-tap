2c2ff0c86fa1fe56899f8e734a9628785d9e8ed3
Chen Asraf <contact@casraf.dev>
2026-02-03 11:15:13 +0000

    cospend-cli: add 1.0.0 bottle.

--- Formula/cospend-cli.rb
+++ Formula/cospend-cli.rb
@@ -5,6 +5,13 @@ class CospendCli < Formula
   sha256 "f17a53f19421fb4fa3ae76c3ebdc418b477c1cda8eb83c31b494897d55547445"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/chenasraf/homebrew-tap/releases/download/cospend-cli-1.0.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c70775bb59738f308c64d6ee2b3eec1e52632f02bba6abcb9de68ddec7a9ef78"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8a547b5af4898a1e2f19116e684e0df9a2ddbfdf2725ed35fb758122d75f585f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "910a22a7a2cefd41fad6a7981415cd7e6ef6844afdb1381e19daf0222456408c"
+  end
+
   depends_on "go" => [:build]
 
   def install
