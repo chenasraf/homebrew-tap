99f282e7d978296a994b0be1f05e4d788fdea5e1
Chen Asraf <casraf@pm.me>
2026-02-03 13:03:15 +0200

    feat: add cospend-cli v1.0.0
    Closes #72.
    
    Signed-off-by: Chen Asraf <contact@casraf.dev>

---
+++ Formula/cospend-cli.rb
@@ -0,0 +1,19 @@
+class CospendCli < Formula
+  desc "Command-line interface for adding expenses to Nextcloud Cospend projects"
+  homepage "https://github.com/chenasraf/cospend-cli"
+  url "https://github.com/chenasraf/cospend-cli/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "f17a53f19421fb4fa3ae76c3ebdc418b477c1cda8eb83c31b494897d55547445"
+  license "MIT"
+
+  depends_on "go" => [:build]
+
+  def install
+    system "go", "build", "-buildmode", "exe", "-o", "cospend", "."
+    bin.install "cospend"
+  end
+
+  test do
+    msg = "cospend is a command-line interface for adding expenses to Nextcloud Cospend projects."
+    assert_match msg, shell_output("#{bin}/cospend -h")
+  end
+end
