ce2c26138ea1aaa7da3428a2dbf4c0375f8a8c5a
Chen Asraf <contact@casraf.dev>
2026-02-09 22:41:16 +0000

    cospend-cli: update 1.4.0 bottle.

--- Formula/cospend-cli.rb
+++ Formula/cospend-cli.rb
@@ -6,10 +6,10 @@ class CospendCli < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/chenasraf/homebrew-tap/releases/download/cospend-cli-1.3.0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d408c54cdc1d38e926dfdbccdc8c20b4d5c089dd26f2c700c91810f00936761d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a9ed5df30e7398015542add23a02098436f0283b9fb13cddda64c5383295c495"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ed2028deafb5de051f051804ceac7aabf5aa2776be7497fe93126f2bf5bb7994"
+    root_url "https://github.com/chenasraf/homebrew-tap/releases/download/cospend-cli-1.4.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "937082f8d93c307cc52d3e47ccb7d48b4c23a19479c9af8656ae42b6f999b72b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bc1ca9132e17ba53cbd094afdf3822f2a104c87ae9af9257b1b6301de8484817"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8ccdee3b1ac03f45359fd059c1e4c89052c5e0bd3c04401a1f78ed76f6f2c50f"
   end
 
   depends_on "go" => [:build]
