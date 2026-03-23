c54a967f0359ea1c317aa48323d708ab9b563463
Chen Asraf <contact@casraf.dev>
2026-03-23 21:25:30 +0000

    tx: update 2.1.0 bottle.

--- Formula/tx.rb
+++ Formula/tx.rb
@@ -6,10 +6,10 @@ class Tx < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/chenasraf/homebrew-tap/releases/download/tx-2.0.0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "919a42bc82b1d145b12e748661de2abaa4da28ba9d6f5d738b41a53c4163855f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ecd6671fc9e252f86aeebc92cf5bfd278052bb5b6cf77d58f9dd3010d1d37357"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dd84ad5f4baa93fee19de140ef30d0130339fb1f20b108dfd83e38b93f0540bc"
+    root_url "https://github.com/chenasraf/homebrew-tap/releases/download/tx-2.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b2989e9a32f522f22721eb48893516888c5d9d08ec96d398cb04454d2a59a1f9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c72757f651789ae389846aaa4999f28e99f3cab8d7cd22c5d641fba7a0494602"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "47c136aeeacbd87d4ced4f185ad4c26b8852e8b399618e54cc73402c4e9f86f5"
   end
 
   depends_on "go" => [:build]
