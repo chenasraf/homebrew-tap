4de75c529fff90a8d9185d96717039f7f15700e1
Chen Asraf <contact@casraf.dev>
2026-05-29 21:53:50 +0000

    wand: update 1.5.0 bottle.

--- Formula/wand.rb
+++ Formula/wand.rb
@@ -6,11 +6,11 @@ class Wand < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/chenasraf/homebrew-tap/releases/download/wand-1.4.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "72cb46de897c2555aeefa18bfa81ac2d6c0c5ce882b28f8bb420120823748998"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "34f7e8afc3af0e5e663e95782de4022a844eabb35469ade53387226fb3acfb67"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f25e2a669f9a41610ee3b9f9e098ac35ef62d3992ead76d0ad4d27d3114eca10"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c0b4de9576b08f38e6e75fd49a4c891c3b98a03c2e99d6c03afc4051c0c414d0"
+    root_url "https://github.com/chenasraf/homebrew-tap/releases/download/wand-1.5.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4d25b353e93ed39fc4d00a01a1ce0d285c91815a807b8abd5d746caf5b3944e7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5e4eb12f5d75c31803f51cca0798444196c91f3441695e5000e49aa9a1db5afd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cd2944946978823749d5de1ee716593310e5c4c00f6ee880f412ee175f2871f1"
+    sha256 cellar: :any,                 x86_64_linux:  "ed7752adae1f289545327afa9c11c3d7096d23ec61a929496bf62762195ed4a8"
   end
 
   depends_on "go" => [:build]
