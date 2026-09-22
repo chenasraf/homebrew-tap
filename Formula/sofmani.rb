bc06276ec71ca19fe065c4237b8fe809af65bf71
Chen Asraf <contact@casraf.dev>
2026-09-22 15:53:36 +0000

    sofmani: update 1.33.1 bottle.

--- Formula/sofmani.rb
+++ Formula/sofmani.rb
@@ -6,11 +6,11 @@ class Sofmani < Formula
   license "CC0-1.0"
 
   bottle do
-    root_url "https://github.com/chenasraf/homebrew-tap/releases/download/sofmani-1.33.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f90828b1b07f3a2e610fa2f19d4cd3de34d58e8da943069673a992281c267efe"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0f3123b02e5d42f309a21914c0f01261d691b5ec518b8ce928a4beace342c4f7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "67c8927dc4ca71413390ceaa6b7c471eb13f47c9a0aa1a220d89d0c8588ab421"
-    sha256 cellar: :any,                 x86_64_linux:  "a73c45554443b91e4c472214f2e12bb9cb76dcae60c5fcf343f1eb4ba3268fe3"
+    root_url "https://github.com/chenasraf/homebrew-tap/releases/download/sofmani-1.33.1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b62237550b47015c09eee34375b4cf7eb21251a04c406026e8c6fea7dd2e052f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c7063c3c30b8d8f39253e72b6f69be53009b88e07b84c5dcc18fe76de4eff734"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "93b2d0d9c9dc9acf92a8c433d5090f7bc1d7134b12fdc2feabd05fef6dde2e8e"
+    sha256 cellar: :any,                 x86_64_linux:  "eb66fc3ad8d253a4d22abe6b8e442cba17e99d15e3a60fe9f4260d0eb83e104d"
   end
 
   depends_on "go" => [:build]
